b668445c1b9ec3603186e0f5041864bfa3835d16
12615az-droid <12615az@gmail.com>
2026-07-09 18:25:46 +0300

    home: source Hyprland Catppuccin palette

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -2,6 +2,7 @@
 
 {
   xdg.configFile."hypr/hyprland.conf".source = ./hypr/hyprland.conf;
+  xdg.configFile."hypr/macchiato.conf".source = ./hypr/macchiato.conf;
 
   xdg.configFile."waybar/config".source = ./waybar/config.jsonc;
   xdg.configFile."waybar/style.css".source = ./waybar/style.css;
