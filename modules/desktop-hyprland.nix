c30362ac56575f4164ca5e7c87c67275bc53edbe
12615az-droid <12615az@gmail.com>
2026-07-09 19:37:16 +0300

    improve hyprland packages and fonts

--- modules/desktop-hyprland.nix
+++ modules/desktop-hyprland.nix
@@ -6,31 +6,41 @@
     xwayland.enable = true;
   };
 
-  # Чтобы Electron-приложения лучше работали через Wayland.
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  # Чтобы Wayland/Electron/Firefox работали стабильнее в Hyprland.
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+    MOZ_ENABLE_WAYLAND = "1";
+  };
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    noto-fonts
+    noto-fonts-emoji
+  ];
 
   environment.systemPackages = with pkgs; [
-      kitty
-      waybar
-      rofi
-      mako
-  
-      grim
-      slurp
-      swappy
-  
-      wl-clipboard
-      cliphist
-  
-      pavucontrol
-      pamixer
-      playerctl
-      brightnessctl
-  
-      hyprpicker
-      hyprsunset
-    ];
+    kitty
+    waybar
+    rofi-wayland
+    mako
+
+    grim
+    slurp
+    swappy
+
+    wl-clipboard
+    cliphist
+
+    pavucontrol
+    pamixer
+    playerctl
+    brightnessctl
+
+    hyprpicker
+    hyprsunset
+
+    libnotify
+    networkmanagerapplet
+  ];
 }
-
-
-
