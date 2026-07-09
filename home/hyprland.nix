a1d88613914cb03a031c387a9e54397c1ea31102
12615az-droid <12615az@gmail.com>
2026-07-09 19:37:09 +0300

    link hyprland desktop configs

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -7,4 +7,9 @@
   xdg.configFile."waybar/style.css".source = ./waybar/style.css;
 
   xdg.configFile."mako/config".source = ./mako/config;
+
+  xdg.configFile."rofi/config.rasi".source = ./rofi/config.rasi;
+  xdg.configFile."rofi/purple-neon.rasi".source = ./rofi/purple-neon.rasi;
+
+  xdg.configFile."kitty/kitty.conf".source = ./kitty/kitty.conf;
 }
